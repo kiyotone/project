05145e04617918f84a1a2cc38a1cc3de7416f96d
kiyotone <kunwarkirtan36@gmail.com>
2022-12-12 17:22:03 +0545

    second push

--- components/NavContents.js
+++ components/NavContents.js
@@ -4,17 +4,15 @@ import React from 'react'
 function NavContents({name,icon}) {
   return (
     
-    <div className=" ml-1 hover:text-[#734BF9] hover:bg-gradient-to-r from-[#734BF9] to-transparent  text-[#090a0a] rounded-l-3xl border-[2.5px] flex flex-col  justify-center  h-14">
+    <div className="ml-10 hover:text-[#734BF9] hover:bg-white hover:border-[#734BF9] hover:border-r-transparent text-[#090a0a] rounded-l-3xl border-[2.5px] flex flex-col  justify-center  h-14">
       
-      <div className="hover:bg-white rounded-l-3xl ml-[1px] flex flex-col  justify-center w-full h-12">
-
-      
+    
         <div className="flex pl-3 items-center">
           <div className="">{icon}</div>
           <div className="ml-2">{name}</div>
         </div>  
-        </div>
       </div>
+      
     )
 }
 
